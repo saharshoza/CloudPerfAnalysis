ce060e40fe3fd3d7c1a4e9515d98fe1c4cfec45f
Saharsh Oza <oza.saharsh@gmail.com>
2016-11-18 22:23:33 -0600

    gather perf on remote as well

--- spark_perf_experiment.sh
+++ spark_perf_experiment.sh
@@ -2,6 +2,10 @@ Workerpid=$1
 memoryArray=(2048 4096 6144 8192)
 unit=M
 mkdir results
+echo "mkdir results" | ssh cc@node-1 /bin/bash
+echo "mkdir results" | ssh cc@node-2 /bin/bash
+echo "mkdir results" | ssh cc@node-3 /bin/bash
+
 for memoryConfig in ${memoryArray[@]}
 do
 	echo "Begin Modifying spark-env.sh"
@@ -22,6 +26,22 @@ do
 	echo "This run is just for execution time"
 	bash $SPARK_BENCH_HOME/Terasort/bin/run.sh
 
+	echo "Get Worker PID on remote machines"
+	node1Worker=`echo "jps | grep Worker" | ssh cc@node-1 /bin/bash | awk {'print$1'}`
+	node2Worker=`echo "jps | grep Worker" | ssh cc@node-2 /bin/bash | awk {'print$1'}`
+	node3Worker=`echo "jps | grep Worker" | ssh cc@node-3 /bin/bash | awk {'print$1'}`
+
+	echo "Spawn perf on remote machines"
+	echo "Node1 spawned"
+	ssh cc@node-1 screen -d -m "sudo perf record -e instructions,cycles,branches,branch-misses -p $node1Worker"
+	node1pid=`echo "ps aux | grep perf | grep root" | ssh cc@node-1 /bin/bash | awk {'print$2'} | head -1`
+	echo "Node2 spawned"
+	ssh cc@node-2 screen -d -m "sudo perf record -e instructions,cycles,branches,branch-misses -p $node2Worker"
+	node2pid=`echo "ps aux | grep perf | grep root" | ssh cc@node-2 /bin/bash | awk {'print$2'} | head -1`
+	echo "Node3 spawned"
+	ssh cc@node-3 screen -d -m "sudo perf record -e instructions,cycles,branches,branch-misses -p $node3Worker"
+	node3pid=`echo "ps aux | grep perf | grep root" | ssh cc@node-3 /bin/bash | awk {'print$2'} | head -1`
+
 	echo "Gather CPI, Branch miss rate"
 	nohup sudo perf record -e instructions,cycles,branches,branch-misses -p $Workerpid >>cpi.out &
 	echo $! > run.pid
@@ -30,6 +50,25 @@ do
 	sudo kill -15 $perfProcess
 	sudo mv perf.data results/perf.data.1."$memoryConfig"
 
+	echo "Clean up on remote machines"
+	echo "sudo kill -15 $node1pid" | ssh cc@node-1 /bin/bash
+	echo "mv perf.data results/perf.data.1."$memoryConfig"" | ssh cc@node-1 /bin/bash
+	echo "sudo kill -15 $node2pid" | ssh cc@node-2 /bin/bash
+	echo "mv perf.data results/perf.data.1."$memoryConfig"" | ssh cc@node-2 /bin/bash
+	echo "sudo kill -15 $node3pid" | ssh cc@node-3 /bin/bash
+	echo "mv perf.data results/perf.data.1."$memoryConfig"" | ssh cc@node-3 /bin/bash
+
+	echo "Spawn perf on remote machines"
+	echo "Node1 spawned"
+	ssh cc@node-1 screen -d -m "sudo perf record -e cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses -p $node1pid"
+	node1pid=`echo "ps aux | grep perf | grep root" | ssh cc@node-1 /bin/bash | awk {'print$2'} | head -1`
+	echo "Node2 spawned"
+	ssh cc@node-2 screen -d -m "sudo perf record -e cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses -p $node2Worker"
+	node2pid=`echo "ps aux | grep perf | grep root" | ssh cc@node-2 /bin/bash | awk {'print$2'} | head -1`
+	echo "Node3 spawned"
+	ssh cc@node-3 screen -d -m "sudo perf record -e cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses -p $node3Worker"
+	node3pid=`echo "ps aux | grep perf | grep root" | ssh cc@node-3 /bin/bash | awk {'print$2'} | head -1`
+
 	echo "Gather Cache miss rate, L1D miss rate"
 	nohup sudo perf record -e cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses -p $Workerpid >>cache.out &
 	echo $! > run.pid
@@ -37,4 +76,13 @@ do
 	bash $SPARK_BENCH_HOME/Terasort/bin/run.sh
 	sudo kill -15 $perfProcess	
 	sudo mv perf.data results/perf.data.2."$memoryConfig"
+
+	echo "Clean up on remote machines"
+	echo "sudo kill -15 $node1pid" | ssh cc@node-1 /bin/bash
+	echo "mv perf.data results/perf.data.2."$memoryConfig"" | ssh cc@node-1 /bin/bash
+	echo "sudo kill -15 $node2pid" | ssh cc@node-2 /bin/bash
+	echo "mv perf.data results/perf.data.2."$memoryConfig"" | ssh cc@node-2 /bin/bash
+	echo "sudo kill -15 $node3pid" | ssh cc@node-3 /bin/bash
+	echo "mv perf.data results/perf.data.2."$memoryConfig"" | ssh cc@node-3 /bin/bash
+
 done
