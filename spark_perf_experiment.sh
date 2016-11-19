c89eb5a6e70471174109c79b531f97d095bb5b85
Saharsh Oza <oza.saharsh@gmail.com>
2016-11-19 11:44:59 -0600

    Changes to make spark work for different num of cores

--- spark_perf_experiment.sh
+++ spark_perf_experiment.sh
@@ -1,11 +1,32 @@
-Workerpid=`jps | grep Worker | awk {'print$1'}`
-echo "Worker ID is "$Workerpid""
+numCores=$1
 memoryArray=(2048 4096 6144 8192)
 unit=M
-mkdir results
-echo "mkdir results" | ssh cc@node-1 /bin/bash
-echo "mkdir results" | ssh cc@node-2 /bin/bash
-echo "mkdir results" | ssh cc@node-3 /bin/bash
+mkdir results"$numCores"
+echo "mkdir results"$numCores"" | ssh cc@node-1 /bin/bash
+echo "mkdir results"$numCores"" | ssh cc@node-2 /bin/bash
+echo "mkdir results"$numCores"" | ssh cc@node-3 /bin/bash
+
+.$SPARK_HOME/sbin/stop-all.sh
+
+echo "Begin Modifying spark-env.sh for $numCores"
+cp $SPARK_HOME/conf/spark-env.sh ./
+sed -i 's/^export SPARK_WORKER_CORES.*$/export SPARK_WORKER_CORES='"$numCores"'/' spark-env.sh
+cp spark-env.sh $SPARK_HOME/conf/
+scp cc@node-1:$SPARK_HOME/conf/spark-env.sh ./
+sed -i 's/^export SPARK_WORKER_CORES.*$/export SPARK_WORKER_CORES='"$numCores"'/' spark-env.sh
+scp spark-env.sh cc@node-1:$SPARK_HOME/conf/
+scp cc@node-2:$SPARK_HOME/conf/spark-env.sh ./
+sed -i 's/^export SPARK_WORKER_CORES.*$/export SPARK_WORKER_CORES='"$numCores"'/' spark-env.sh
+scp spark-env.sh cc@node-2:$SPARK_HOME/conf/
+scp cc@node-3:$SPARK_HOME/conf/spark-env.sh ./
+sed -i 's/^export SPARK_WORKER_CORES.*$/export SPARK_WORKER_CORES='"$numCores"'/' spark-env.sh
+scp spark-env.sh cc@node-3:$SPARK_HOME/conf/
+echo "End Modifying spark-env.sh"
+
+.$SPARK_HOME/sbin/start-all.sh
+
+Workerpid=`jps | grep Worker | awk {'print$1'}`
+echo "Worker ID is "$Workerpid""
 
 for memoryConfig in ${memoryArray[@]}
 do
@@ -52,15 +73,15 @@ do
 	perfProcess=`cat run.pid`
 	bash $SPARK_BENCH_HOME/Terasort/bin/run.sh
 	sudo kill -15 $perfProcess
-	sudo mv perf.data results/perf.data.1."$memoryConfig"
+	sudo mv perf.data results"$numCores"/perf.data.1."$memoryConfig"
 
 	echo "Clean up on remote machines"
 	echo "sudo kill -15 $node1pid" | ssh cc@node-1 /bin/bash
-	echo "mv perf.data results/perf.data.1."$memoryConfig"" | ssh cc@node-1 /bin/bash
+	echo "mv perf.data results"$numCores"/perf.data.1."$memoryConfig"" | ssh cc@node-1 /bin/bash
 	echo "sudo kill -15 $node2pid" | ssh cc@node-2 /bin/bash
-	echo "mv perf.data results/perf.data.1."$memoryConfig"" | ssh cc@node-2 /bin/bash
+	echo "mv perf.data results"$numCores"/perf.data.1."$memoryConfig"" | ssh cc@node-2 /bin/bash
 	echo "sudo kill -15 $node3pid" | ssh cc@node-3 /bin/bash
-	echo "mv perf.data results/perf.data.1."$memoryConfig"" | ssh cc@node-3 /bin/bash
+	echo "mv perf.data results"$numCores"/perf.data.1."$memoryConfig"" | ssh cc@node-3 /bin/bash
 
 	echo "Spawn perf on remote machines"
 	ssh cc@node-1 screen -d -m "sudo perf record -e cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses -p $node1pid"
@@ -79,13 +100,15 @@ do
 	perfProcess=`cat run.pid`
 	bash $SPARK_BENCH_HOME/Terasort/bin/run.sh
 	sudo kill -15 $perfProcess	
-	sudo mv perf.data results/perf.data.2."$memoryConfig"
+	sudo mv perf.data results"$numCores"/perf.data.2."$memoryConfig"
 
 	echo "Clean up on remote machines"
 	echo "sudo kill -15 $node1pid" | ssh cc@node-1 /bin/bash
-	echo "mv perf.data results/perf.data.2."$memoryConfig"" | ssh cc@node-1 /bin/bash
+	echo "mv perf.data results"$numCores"/perf.data.2."$memoryConfig"" | ssh cc@node-1 /bin/bash
 	echo "sudo kill -15 $node2pid" | ssh cc@node-2 /bin/bash
-	echo "mv perf.data results/perf.data.2."$memoryConfig"" | ssh cc@node-2 /bin/bash
+	echo "mv perf.data results"$numCores"/perf.data.2."$memoryConfig"" | ssh cc@node-2 /bin/bash
 	echo "sudo kill -15 $node3pid" | ssh cc@node-3 /bin/bash
-	echo "mv perf.data results/perf.data.2."$memoryConfig"" | ssh cc@node-3 /bin/bash
+	echo "mv perf.data results"$numCores"/perf.data.2."$memoryConfig"" | ssh cc@node-3 /bin/bash
 done
+
+sudo mv /tmp/spark-events /tmp/spark-events"$numCores"
