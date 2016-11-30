4cec51531c9eb6cad50d3e0d0d4dbd76467795be
Saharsh Oza <oza.saharsh@gmail.com>
2016-11-30 13:17:32 -0600

    Correct grep

--- perf_stat_metrics.sh
+++ perf_stat_metrics.sh
@@ -12,7 +12,7 @@ do
 done
 
 grepList=(instructions:uk cycles:uk branches:uk branch-misses:uk L1-dcache-load-misses L1-dcache-loads L1-dcache-stores L1-dcache-store-misses dTLB-loads dTLB-load-misses dTLB-stores dTLB-store-misses LLC-loads LLC-stores)
-fileList=(cpi.out cpi.out branch.out branch.out l1dcache_loads.out l1dcache_loads.out l1dcache_stores.out l1dcache_stores.out dtlb_loads.out dtlb_loads.out llc.out llc.out)
+fileList=(cpi.out cpi.out branch.out branch.out l1dcache_loads.out l1dcache_loads.out l1dcache_stores.out l1dcache_stores.out dtlb_loads.out dtlb_loads.out dtlb_stores.out dtlb_stores.out llc.out llc.out)
 lenList=${#fileList[@]}
 for i in {0..12}
 do
