cc4f3d990e2a5be07c3c92f0c3d4fc2b5014c7a0
Saharsh Oza <oza.saharsh@gmail.com>
2016-11-29 20:12:55 -0600

    process the logs

--- perf_stat_metrics.sh
+++ perf_stat_metrics.sh
@@ -10,3 +10,12 @@ do
 	bash $SPARK_BENCH_HOME/Terasort/bin/run.sh
 	kill $perf_process
 done
+
+grepList=(instructions:uk cycles:uk branches:uk branch-misses:uk L1-dcache-load-misses L1-dcache-loads L1-dcache-stores L1-dcache-store-misses dTLB-loads dTLB-load-misses dTLB-stores dTLB-store-misses LLC-loads LLC-stores)
+fileList=(cpi.out cpi.out branch.out branch.out l1dcache_loads.out l1dcache_loads.out l1dcache_stores.out l1dcache_stores.out dtlb_loads.out dtlb_loads.out llc.out llc.out)
+lenList=${#fileList[@]}
+for i in {1..lenList}
+do
+	echo ${grepList[i]}
+	grep -i ${grepIter} $fileList | awk '{print $2}' > $grepIter.out
+done
