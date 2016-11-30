2c932d27ea9783c4258f05974600c2ae7830e589
Saharsh Oza <oza.saharsh@gmail.com>
2016-11-29 20:16:47 -0600

    process the logs

--- perf_stat_metrics.sh
+++ perf_stat_metrics.sh
@@ -14,8 +14,8 @@ done
 grepList=(instructions:uk cycles:uk branches:uk branch-misses:uk L1-dcache-load-misses L1-dcache-loads L1-dcache-stores L1-dcache-store-misses dTLB-loads dTLB-load-misses dTLB-stores dTLB-store-misses LLC-loads LLC-stores)
 fileList=(cpi.out cpi.out branch.out branch.out l1dcache_loads.out l1dcache_loads.out l1dcache_stores.out l1dcache_stores.out dtlb_loads.out dtlb_loads.out llc.out llc.out)
 lenList=${#fileList[@]}
-for i in {1..lenList}
+for i in {1..12}
 do
 	echo ${grepList[i]}
-	grep -i ${grepIter} $fileList | awk '{print $2}' > $grepIter.out
+	grep -i ${grepList[i]} ${fileList[i]} | awk '{print $1}' > ${grepList[i]}.out
 done
